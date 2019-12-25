a503945814f5de1ee95994b87f3cc4a64ef73731
sulman shaikh <sulmanmh@gmail.com>
2019-12-25 21:48:23 +0000

    Added go routine for stock symbol Get

--- stockSymbolsByIndustry.go
+++ stockSymbolsByIndustry.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 	"regexp"
 	"net/http"
-	// "io"
-	// "os"
 	"io/ioutil"
 	"strings"
 	"encoding/json"
 	"time"
+	"sync"
 )
 
 type StockSymbol struct{
@@ -23,55 +22,101 @@ type StockSymbol struct{
  	Agriculture string = "WSJMXUSAGRI"
  )
 
-// func getStockSymbols(industry string)
-
- func main() {
- 	start := time.Now()
+func getStockSymbols(industry string, page int, wg *sync.WaitGroup) {
+	start := time.Now()
  	Symbols := []StockSymbol{}
- 	page := 0
- 	urlToRead := fmt.Sprintf(Url, Agriculture, page)
+ 	urlToRead := fmt.Sprintf(Url, industry, page)
  	resp, err := http.Get(urlToRead)
  	if err != nil {
  		fmt.Printf("Could  not read url: %s with error: %v\n", urlToRead, err)
  	}
- 	 b, err := ioutil.ReadAll(resp.Body)
+ 	b, err := ioutil.ReadAll(resp.Body)
  	if err != nil {
  		fmt.Printf("Could not read as bytes: %v\n", err)
  	}
  	re := regexp.MustCompile(Pattern)
- 	results := re.FindAll(b, -1)
+	for _, res := range re.FindAll(b, -1) {
+ 		s := fmt.Sprintf("%s", res)
+ 		i := strings.Index(s, ">") + 1
+ 		j := strings.Index(s, "</td>")
+ 		x := strings.Index(s, "<div>") + 5
+ 		y := strings.Index(s, "</div>")
+ 		Symbols = append(Symbols,StockSymbol{Code: s[i:j], Description: s[x:y]})
+	}
 
- 	for len(results) > 0 {
- 		for _, res := range results {
-	 		s := fmt.Sprintf("%s", res)
-	 		i := strings.Index(s, ">") + 1
-	 		j := strings.Index(s, "</td>")
-	 		x := strings.Index(s, "<div>") + 5
-	 		y := strings.Index(s, "</div>")
-	 		Symbols = append(Symbols,StockSymbol{Code: s[i:j], Description: s[x:y]})
- 		}
-
- 		page += 50
- 		urlToRead = fmt.Sprintf(Url, Agriculture, page)
- 		// fmt.Println(urlToRead)
- 		resp, err = http.Get(urlToRead)
- 		if err != nil {
- 			fmt.Printf("Could  not read url: %s with error: %v\n", urlToRead, err)
- 		}
-
- 		b, err := ioutil.ReadAll(resp.Body)
- 		if err != nil {
- 			fmt.Printf("Could not read as bytes: %v\n", err)
- 		}
- 		results = re.FindAll(b, -1)
- 		// fmt.Println(resp.Status)
- 	}
  	resp.Body.Close()
  	data, err := json.Marshal(Symbols)
  	if err != nil {
  		fmt.Println("Could not marshal struct to JSON")
  	}
  	elapsed := time.Since(start)
- 	fmt.Printf("Number of symbols %d too total of: %s \n", len(data), elapsed)
+ 	fmt.Printf("Number of symbols %d time of: %s page: %d \n", len(data), elapsed, page)
+ 	wg.Done()
+}
+
+ func main() {
+ 	mainStart := time.Now()
+	
+	defer func() {
+		mainElapsed := time.Since(mainStart)
+		fmt.Printf("\nTotal time: %s\n", mainElapsed)
+	}()
+
+ 	pages := []int{0, 50, 100, 150, 200}
+ 	var wg sync.WaitGroup
+ 	wg.Add(len(pages))
+
+ 	for _, page := range pages {
+ 		go getStockSymbols(Agriculture, page, &wg)
+ 	}
+
+ 	wg.Wait()
+ 	// start := time.Now()
+ 	// Symbols := []StockSymbol{}
+ 	// page := 0
+ 	// urlToRead := fmt.Sprintf(Url, Agriculture, page)
+ 	// resp, err := http.Get(urlToRead)
+ 	// if err != nil {
+ 	// 	fmt.Printf("Could  not read url: %s with error: %v\n", urlToRead, err)
+ 	// }
+ 	//  b, err := ioutil.ReadAll(resp.Body)
+ 	// if err != nil {
+ 	// 	fmt.Printf("Could not read as bytes: %v\n", err)
+ 	// }
+ 	// re := regexp.MustCompile(Pattern)
+ 	// results := re.FindAll(b, -1)
+
+ 	// for len(results) > 0 {
+ 	// 	for _, res := range results {
+	 // 		s := fmt.Sprintf("%s", res)
+	 // 		i := strings.Index(s, ">") + 1
+	 // 		j := strings.Index(s, "</td>")
+	 // 		x := strings.Index(s, "<div>") + 5
+	 // 		y := strings.Index(s, "</div>")
+	 // 		Symbols = append(Symbols,StockSymbol{Code: s[i:j], Description: s[x:y]})
+ 	// 	}
+
+ 	// 	page += 50
+ 	// 	urlToRead = fmt.Sprintf(Url, Agriculture, page)
+ 	// 	// fmt.Println(urlToRead)
+ 	// 	resp, err = http.Get(urlToRead)
+ 	// 	if err != nil {
+ 	// 		fmt.Printf("Could  not read url: %s with error: %v\n", urlToRead, err)
+ 	// 	}
+
+ 	// 	b, err := ioutil.ReadAll(resp.Body)
+ 	// 	if err != nil {
+ 	// 		fmt.Printf("Could not read as bytes: %v\n", err)
+ 	// 	}
+ 	// 	results = re.FindAll(b, -1)
+ 	// 	// fmt.Println(resp.Status)
+ 	// }
+ 	// resp.Body.Close()
+ 	// data, err := json.Marshal(Symbols)
+ 	// if err != nil {
+ 	// 	fmt.Println("Could not marshal struct to JSON")
+ 	// }
+ 	// elapsed := time.Since(start)
+ 	// fmt.Printf("Number of symbols %d too total of: %s \n", len(data), elapsed)
 
  }
